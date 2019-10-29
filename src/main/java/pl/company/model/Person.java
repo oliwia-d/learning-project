1d4f50f1ad3f3592aa2eceb4270c061f45417fe6
Oliwia Dykas <dykas.oliwia@gmail.com>
2019-10-29 20:15:47 +0100

    Added guinea pig model

--- src/main/java/pl/company/model/Person.java
+++ src/main/java/pl/company/model/Person.java
@@ -5,11 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -33,6 +37,8 @@ public class Person implements Serializable {
     @Min(value = 1)
     @Column(name = "age")
     private int age;
+    @OneToMany(mappedBy = "person")
+    private List<GuineaPig> piggies;
 
     public String getName() {
         return name;
@@ -56,6 +62,14 @@ public class Person implements Serializable {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public List<GuineaPig> getPiggies() {
+        return piggies;
+    }
+
+    public void setPiggies(List<GuineaPig> piggies) {
+        this.piggies = piggies;
     }
 
     @Override

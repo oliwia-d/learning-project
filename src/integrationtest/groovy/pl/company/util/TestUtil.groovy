5c5a3150c38609b73266dcab97baa385afecbb92
oliwia-d <57099724+oliwia-d@users.noreply.github.com>
2020-02-06 17:32:15 +0100

    cleanup: changed visibility from public to package private for test utility

--- src/integrationtest/groovy/pl/company/util/TestUtil.groovy
+++ src/integrationtest/groovy/pl/company/util/TestUtil.groovy
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class TestUtil {
 
-    public static String asJsonString(final Object obj) {
+    static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj)
         } catch (Exception e) {

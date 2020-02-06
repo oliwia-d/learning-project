65821ac1a7cde37eeaf72f43511b7308a28ce752
oliwia-d <57099724+oliwia-d@users.noreply.github.com>
2020-02-06 16:58:59 +0100

    cleanup: changed type of age from int to Integer, added all arguments constructor to PersonCommand

--- src/main/java/pl/company/application/person/command/AddPersonCommand.java
+++ src/main/java/pl/company/application/person/command/AddPersonCommand.java
@@ -1,11 +1,15 @@
 package pl.company.application.person.command;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class AddPersonCommand extends PersonCommand {
+
+    @Builder
+    public AddPersonCommand(String name, String surname, Integer age) {
+        super(name, surname, age);
+    }
 }
--- src/main/java/pl/company/application/person/command/UpdatePersonCommand.java
+++ src/main/java/pl/company/application/person/command/UpdatePersonCommand.java
@@ -1,16 +1,19 @@
 package pl.company.application.person.command;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class UpdatePersonCommand extends PersonCommand {
 
     @NotNull
-    private long id;
+    private Long id;
+
+    public UpdatePersonCommand(String name, String surname, Integer age, Long id) {
+        super(name, surname, age);
+        this.id = id;
+    }
 }
